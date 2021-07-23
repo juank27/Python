cf810ba1285d6b24a81ae923b808016659b043d9
juank27 <juankruiz27@gmail.com>
2021-07-23 12:44:57 -0500

    operadores y cambiar tipo de dato

--- basico/basico.py
+++ basico/basico.py
@@ -17,3 +17,24 @@ n1 = 5.0
 #boolean
 estado = True
 estado2 =False
+#input - ingreso de datos por terminal
+numero1 = input("Escribir un numero: ")
+numero1 = int(numero1) #convertir a entero
+texto = str(numero1) #pasar de int a text
+
+#operadores logicos y de compracion
+es_estudiante = True
+trabaja = False
+#operador and para que sea true las dos se deben cumplir
+accion = es_estudiante and trabaja
+#operador or para que sea falso las dos son falsas
+accion = es_estudiante or trabaja
+#not negacion, cambia su estado
+accion = not trabaja
+# igualdad == verofica que los dos sean igual return True
+number1 = 5
+number2 = 5
+number1 == number2
+# diferente != retorna true si los dos son distintos
+number1 != number2
+#operador > < >= <=
