78953a6b36c693525cb840ce5e5384d97fa2a54a
juank27 <juankruiz27@gmail.com>
2021-07-23 12:28:42 -0500

    Tipos de datos

---
+++ basico/basico.py
@@ -0,0 +1,19 @@
+#tipos de datos sencillos
+#String
+nombre = "Juan"
+nombre2 = "Carlos"
+#concatenar cadenas 
+nombre_completo = nombre + nombre2
+
+#char
+caracter = 'c'
+
+#int
+n = 5
+
+#float
+n1 = 5.0
+
+#boolean
+estado = True
+estado2 =False
