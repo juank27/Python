7440911a9f3b853d64f1f9a92c4d28bd6b38fb14
juank27 <juankruiz27@gmail.com>
2021-08-05 22:40:02 -0500

    Funciones anidades y terminos de closure

---
+++ profesional/Funcion_Anidada.py
@@ -0,0 +1,14 @@
+def main():
+   a = 1
+   def nested(): #funcion anidada que solo pertenece a main
+      print(a)
+      for i in range(1,9):
+         print(i)
+   nested() #para poder ser utilizada debe ser retornada.
+   return nested
+
+my_func = main()
+my_func() #realiza el contenido de nested    
+
+del(main) #elimina la funcion 
+my_func() #utilidad del closure recordando la funcion
