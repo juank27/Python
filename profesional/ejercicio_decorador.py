88da871f5050fc5eea76ab35a95d6510c6b8c6b7
juank27 <juankruiz27@gmail.com>
2021-08-07 21:57:52 -0500

    Funcion con decorador que cuenta el tiempo de tardanza de ejecucion de una funcion

---
+++ profesional/ejercicio_decorador.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+#muestra el tiempo de ejecucion de una funcion
+def execution_time(func):
+   def wrapper():
+      #fecha y hora exacta en el momento que se ejecuta la linea de codigo
+      initial_time = datetime.now()
+      func()
+      final_time = datetime.now()
+      time_elapsed = final_time - initial_time
+      print("Pasaron " + str(time_elapsed.total_seconds()) + " segundos")
+   return wrapper
+
+@execution_time
+def random_func():
+   for _ in range(1, 1000000000):
+      pass
+
+random_func()
