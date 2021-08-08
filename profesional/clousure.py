4baecee59f388cd996ed754783ad514a996aab7e
juank27 <juankruiz27@gmail.com>
2021-08-07 21:03:24 -0500

    closure ejercicio de multiplicar palabras

--- profesional/clousure.py
+++ profesional/clousure.py
@@ -12,3 +12,14 @@ print(times10(3))
 print(times4(5))
 print(times10(times4(2)))
 print(n)
+
+#Multiplicar un string por un numero
+def palabra(string:str):
+   assert type(string) == str, "Se deben utilizar cadenas "
+   def multiplicador(n : int):
+      return string * n
+   return multiplicador
+
+name = palabra("Juan")
+numero = name(3)
+print(numero)
