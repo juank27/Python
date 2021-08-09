c31cb4995a371d1e4c9e51d472a989d4e5f5ff9e
juank27 <juankruiz27@gmail.com>
2021-08-09 11:21:05 -0500

    modificacion del iterador fibonacci, para que sea finito e infinito

--- profesional/Estructura_Datos/iterador_fibonacci.py
+++ profesional/Estructura_Datos/iterador_fibonacci.py
@@ -2,6 +2,12 @@ import time
 
 class FiboIter():
    
+   def __init__(selft, max = None):
+      if max == None:
+         selft.max = None
+      else:
+         selft.max = max
+   
    def __iter__(selft):
       selft.n1 = 0
       selft.n2 = 1
@@ -9,22 +15,32 @@ class FiboIter():
       return selft
    
    def __next__(selft):
-      if selft.counter == 0:
-         selft.counter += 1
-         return selft.n1
-      elif selft.counter == 1:
-         selft.counter += 1
-         return selft.n2
-      else:
+      def fibo(): #funcion fibonacci
          selft.aux = selft.n1 + selft.n2
          #selft.n1 = selft.n2
          #selft.n2 = selft.aux
          selft.n1, selft.n2 = selft.n2, selft.aux
          selft.counter += 1
          return selft.aux
+      
+      if selft.counter == 0:
+         selft.counter += 1
+         return selft.n1
+      elif selft.counter == 1:
+         selft.counter += 1
+         return selft.n2
+      elif selft.max == None:
+         return fibo() #ejecucion infinita
+      elif selft.counter >= selft.max:
+         raise StopIteration
+      else:
+         return fibo()#ejecucion controlada
 
 if __name__ == "__main__":
    fibonacci = FiboIter()
+   i  = 0
    for element in fibonacci:
+      print(str(i) + " Vueltas")
       print(element)
       time.sleep(0.05)
+      i += 1
